040e271d927a363cd8ee215cf4d840a5a7e50b51
Clement Lakhal <clementlakhal@gmail.com>
2018-11-20 12:45:36 +0100

    Added PuzzleManager artifact

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,9 +2,11 @@
 /* global artifacts */
 
 const PlayerScore = artifacts.require('PlayerScore');
+const PuzzleManager = artifacts.require('PuzzleManager');
 
 function deployContracts(deployer) {
   deployer.deploy(PlayerScore);
+  deployer.deploy(PuzzleManager);
 }
 
 module.exports = deployContracts;
