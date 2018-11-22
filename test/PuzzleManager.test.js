aebf3b3b5d8ec61656491eb23de8066ef26907c4
Clement Lakhal <clementlakhal@gmail.com>
2018-11-22 02:40:26 +0100

    Added tests

--- test/PuzzleManager.test.js
+++ test/PuzzleManager.test.js
@@ -14,4 +14,70 @@ contract('PuzzleManager', (accounts) => {
     .then((deployed) => {
       instance = deployed;
     }));
+
+  /**
+   * Tests for a secure puzzle
+   */
+
+  it('Should create a new secure puzzle', () => instance.createSecurePuzzle(
+    accounts[0],
+    'puzzle',
+    web3.utils.utf8ToHex('puzzle'),
+    true,
+    'uniqueId',
+  ));
+
+  it('Should add secure metrics to puzzle 0 from account #1', () => instance.pushSecureMetrics(
+    0,
+    web3.utils.utf8ToHex('puzzle'), {
+      from: accounts[1],
+    },
+  ));
+
+  it('Should compare secure metrics for puzzle 0 from account #1', () => instance.compareSecureMetrics(
+    0,
+    true, {
+      from: accounts[1],
+    },
+  )
+    .then((result) => {
+      assert.equal(result, true, 'Metrics are wrong');
+    }));
+
+  it('Should add secure metrics to puzzle 0 from account #2', () => instance.pushSecureMetrics(
+    0,
+    web3.utils.utf8ToHex('puzzle2'), {
+      from: accounts[2],
+    },
+  ));
+
+  it('Should compare secure metrics for puzzle 0', () => instance.compareSecureMetrics(
+    0,
+    true, {
+      from: accounts[2],
+    },
+  )
+    .then((result) => {
+      assert.equal(result, false, 'Metrics are wrong');
+    }));
+
+  /**
+   * Tests for a non-secure puzzle
+   */
+
+  it('Should create a non-secure puzzle', () => instance.createPuzzle(
+    'puzzle',
+    'uniqueId',
+  ));
+
+  it('Should ban account #0', () => instance.ban(accounts[0]));
+
+  it('Should NOT create a non-secure puzzle', () => instance.createPuzzle(
+    'puzzle',
+    'uniqueId',
+  )
+    .then(assert.fail)
+    .catch((error) => {
+      assert.include(error.message, 'Player is banned', 'Create puzzle function should revert');
+    }));
 });
